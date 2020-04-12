98de3c14fe8828f61318751636ac7a6dd4488413
Jaime Pokhoy <jaimepokhoy@gmail.com>
2020-04-12 15:03:02 -0400

    Updates

--- precache-manifest.7aa585d285a6ec5034d10e77bf7812e4.js
+++ precache-manifest.7aa585d285a6ec5034d10e77bf7812e4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4768847769a1cd7081565fd6b630b572",
+    "revision": "6bbbc44cd377ee386fb22a8bfe93d2c2",
     "url": "/connect-four/index.html"
   },
   {
-    "revision": "b612130431fc0df2b4c8",
+    "revision": "9b8130f6f7bad889bd8a",
     "url": "/connect-four/static/css/main.16a0ddcd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/connect-four/static/js/2.92bb8b4d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b612130431fc0df2b4c8",
-    "url": "/connect-four/static/js/main.e01a53c7.chunk.js"
+    "revision": "9b8130f6f7bad889bd8a",
+    "url": "/connect-four/static/js/main.ddff641c.chunk.js"
   },
   {
     "revision": "da047faa124cc3517598",
